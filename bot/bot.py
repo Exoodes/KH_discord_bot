4d2bbb632fde87ee4829839cf98bd4864c245441
Exoodes <jakub.maca.01@gmail.com>
2024-02-08 00:26:06 +0100

    feat: add logging to commands and cog initialization

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from disnake import Activity, ActivityType
 from disnake.ext import commands
+from utils.context import Context
 
 log = logging.getLogger(__name__)
 
@@ -15,3 +16,26 @@ class KH_BOT(commands.Bot):
 
     async def on_ready(self) -> None:
         log.info("Bot is now all ready to go")
+
+    async def on_command(self, ctx: Context) -> None:
+        if ctx.message.content:
+            command = ctx.message.content
+            log.info(
+                f'in #{ctx.channel} @{ctx.author} used command: {command}')
+        else:
+            params = ' '.join(map(str, ctx.kwargs.values()))
+            command = f"{ctx.prefix}{ctx.command} {params}"
+            log.info(
+                f'in #{ctx.channel} @{ctx.author} used slash command: {command}')
+
+    async def on_error(self, event_method: str, *args: Any, **kwargs: Any) -> None:
+        """ reimplement on_error method to print to a log file instead of sys.stderr"""
+        log.error(f'Ignoring exception in %s' % (event_method,), exc_info=True)
+
+    def add_cog(self, cog: commands.Cog, *, override: bool = False) -> None:
+        log.info("loading cog: %s", cog.qualified_name)
+        super().add_cog(cog, override=override)
+
+    def remove_cog(self, name: str) -> None:
+        log.info("unloading cog: %s", name)
+        super().remove_cog(name)
